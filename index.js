5dd5fbfe72f1e92da3e5493fc073ccf0d9b9ccf0
zeff96 <zeffo1975@gmail.com>
2023-02-01 01:33:52 +0300

    Add dynamic support for card creation.

--- index.js
+++ index.js
@@ -23,22 +23,22 @@ const cardArray = [
     title: 'tonic',
     img: 'snapshot-portfolio/tonic.png',
     text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
   },{
     title: 'Multi-post stories',
     img: 'snapshot-portfolio/Multi-post.png',
     text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
   },{
     title: 'tonic',
     img: 'snapshot-portfolio/Snapshoot Portfolio.png',
     text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
   },{
     title: 'Multi-post stories',
     img: 'snapshot-portfolio/Snapshoot .png',
     text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
   }
 ]
 
@@ -63,7 +63,6 @@ const tech = `
     <li>javascript</li>
   </ul>
 `;
-
 
 for (const i in cardArray) {
   const card = document.createElement('div');
@@ -93,10 +92,66 @@ for (const i in cardArray) {
   buttonEl.classList.add('btn');
   buttonEl.innerHTML = 'see project'
 
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+
+  const header = document.createElement('h2');
+  header.classList.add('work-title')
+  header.innerHTML = cardArray[i].title;
+
+  const popFrame = document.createElement('div');
+  popFrame.classList.add('frame');
+  popFrame.innerHTML = frameInfo;
+
+  const popContainer = document.createElement('div');
+  popContainer.classList.add('pop-container')
+  const popimg = document.createElement('img');
+  popimg.setAttribute('src', cardArray[i].img);
+  popContainer.appendChild(popimg);
+
+  const lang = document.createElement('ul');
+  lang.classList.add('tags');
+  lang.innerHTML = tech;
+
+  const txt = document.createElement('p');
+  txt.classList.add('pop-para');
+  txt.innerHTML = cardArray[i].description;
+  
+  const btnLive = document.createElement('button');
+  btnLive.classList.add('btn');
+  btnLive.innerHTML = 'see live';
+  const btnSrc = document.createElement('button');
+  btnSrc.classList.add('btn');
+  btnSrc.innerHTML = 'see source';
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('btn-container');
+  btnContainer.append(btnLive, btnSrc);
+  const rightBlock = document.createElement('div');
+  rightBlock.classList.add('right-block');
+  rightBlock.append(lang, btnContainer)
+
+  const popLeftBlock = document.createElement('div');
+  const closeMark = document.createElement('div');
+  closeMark.classList.add['close'];
+  const dlt = document.createElement('img');
+  dlt.classList.add('xmark');
+  dlt.setAttribute('src', 'icons/dlt-96.png')
+  closeMark.appendChild(dlt);
+  popLeftBlock.classList.add('left-blockpop');
+  popLeftBlock.append(txt,rightBlock)
+  popup.append(closeMark, header, popFrame, popContainer, popLeftBlock)
+
 
   leftBlock.append(title, frame, para, tags, buttonEl)
 
-  card.append(imgContainer, leftBlock)
+  card.append(popup,imgContainer, leftBlock)
   sectionEl.appendChild(card);
 
+  buttonEl.addEventListener('click', () => {
+    popup.classList.toggle('show')
+  })
+
+  dlt.addEventListener('click', () =>{
+    popup.classList.toggle('show');
+  })
 }
