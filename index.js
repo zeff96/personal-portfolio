260e0ac2ab80e2943d14aa3885b5eea1c57bd3cc
zeff96 <zeffo1975@gmail.com>
2023-01-31 18:41:54 +0300

    Add fix to previous wrong commit.

--- index.js
+++ index.js
@@ -16,45 +16,87 @@ linksEl.forEach((link) => {
   })
 })
 
+const sectionEl = document.querySelector('.work');
+
 const cardArray = [
   {
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    title: 'tonic',
+    img: 'snapshot-portfolio/tonic.png',
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
   },{
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    title: 'Multi-post stories',
+    img: 'snapshot-portfolio/Multi-post.png',
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
   },{
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    title: 'tonic',
+    img: 'snapshot-portfolio/Snapshoot Portfolio.png',
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
   },{
-    title: 'post-stories',
-    img: 'img/Img Placeholder.png"',
-    text: "A daily selection of privately personalized reads; no accounts orsign-ups required. has been the industry's standar dummy text eversince the 1500s, when an unknown printer took a standard dummy text",
+    title: 'Multi-post stories',
+    img: 'snapshot-portfolio/Snapshoot .png',
+    text: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
     description: '  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae veritatis velit adipisci numquam nulla fugit. In accusamus quis eveniet nesciunt deserunt aspernatur recusandae voluptate similique molestias sed accusantium saepe ullam excepturi aperiam tempora qui iste quisquam, incidunt vitae voluptatum praesentium explicabo quae repellendus? Quas sapiente tenetur ea! Necessitatibus minima labore dolor soluta in voluptatem aspernatur officiis culpa cupiditate quo repudiandae ducimus, aperiam facilis fugit atque quibusdam ut quaerat omnis praesentium id consectetur fugiat reprehenderit unde? Dolore expedita debitis error assumenda aliquam, enim iste eligendi, odio reiciendis eos reprehenderit ab! Enim dolor rem ducimus, libero quaerat dolorum rerum deleniti veniam consectetur!'
   }
 ]
 
+const frameInfo = `
+  <p class="client">
+    canopy
+  </p>
+  <img src="snapshot-portfolio/Counter.png" alt="counter">
+  <p class="role">
+    back end dev
+  </p>
+  <img src="snapshot-portfolio/Counter.png" alt="counter">
+  <p class="year">
+    2015
+  </p>
+`;
+
+const tech = `
+  <ul>
+    <li>html</li>
+    <li>css</li>
+    <li>javascript</li>
+  </ul>
+`;
+
 
 for (const i in cardArray) {
   const card = document.createElement('div');
-  card.classList.add('card-container')
-  const article = document.createElement('article');
-  article.classList.add('card')
-  const cardBlock = document.createElement('div');
-  cardBlock.classList.add('card-2')
-  const cardImage = document.createElement('div');
-  const h3 = document.createElement('h3');
-  h3.innerHTML = 'Profesional Art Printing Data';
+  card.classList.add('card-work')
 
-  cardBlock.append(h3);
-  article.appendChild(cardBlock);
-  card.append(article);
+  const imgContainer = document.createElement('div');
+  imgContainer.classList.add('snapshot-portfolio')
+  const img = document.createElement('img');
+  img.setAttribute('src', cardArray[i].img);
+  imgContainer.appendChild(img);
 
+  const leftBlock = document.createElement('div');
+  leftBlock.classList.add('left-block')
+  const title = document.createElement('h2');
+  title.classList.add('work-title')
+  title.innerHTML = cardArray[i].title;
+  const frame = document.createElement('div');
+  frame.classList.add('frame');
+  frame.innerHTML = frameInfo;
+  const para = document.createElement('p');
+  para.classList.add('para');
+  para.innerHTML = cardArray[i].text;
+  const tags = document.createElement('div');
+  tags.classList.add('tags');
+  tags.innerHTML = tech;
+  const buttonEl = document.createElement('button');
+  buttonEl.classList.add('btn');
+  buttonEl.innerHTML = 'see project'
+
+
+  leftBlock.append(title, frame, para, tags, buttonEl)
+
+  card.append(imgContainer, leftBlock)
   sectionEl.appendChild(card);
+
 }
